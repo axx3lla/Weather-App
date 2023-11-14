4d4817574199b128c5ad8156c0d95cc92dbf86ee
Alexandra Dascalu <alexandradascalu145@gmail.com>
2023-11-14 22:56:39 +0200

    forecasted | temp | descrption | noHour|

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputCityEl = document.getElementById("cityInput");
 const searchBtnEl = document.querySelector(".search-button");
+const forecastBtnEl = document.querySelector(".forecast-button");
 const locationEl = document.querySelector(".location");
 const humidityEl = document.querySelector(".humidity");
 const windEl = document.querySelector(".wind");
@@ -9,9 +10,18 @@ const minTempEl = document.querySelector(".min-temperature");
 const maxTempEl = document.querySelector(".max-temperature");
 const weatherNowContainerEl = document.querySelector(".weather-now");
 const iconEl = document.querySelector(".icon");
+const forecastedWeatherContainer = document.querySelector(
+  ".forecasted-weather"
+);
+const dayForecastedEl = document.querySelector(".day");
 
 const currentWeatherURL =
   "https://api.openweathermap.org/data/2.5/weather?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=";
+
+const forecastedWeatherURL =
+  "https://api.openweathermap.org/data/2.5/forecast?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=";
+
+const iconWeatherURL = "http://openweathermap.org/img/w/";
 
 searchBtnEl.addEventListener("click", () => {
   const location = inputCityEl.value;
@@ -20,20 +30,19 @@ searchBtnEl.addEventListener("click", () => {
   }
 });
 
-const iconWeatherURL = "http://openweathermap.org/img/w/";
-
 function showCurrentWeather(location) {
   const url = `${currentWeatherURL}${location}`;
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      weatherNowContainerEl.style.display = "flex";
       locationEl.innerHTML = data.name;
       currentTempEl.innerHTML = `${Math.round(data.main.temp)}° C`;
       humidityEl.innerHTML = `Humidity: ${data.main.humidity} %`;
       windEl.innerHTML = `Wind: ${data.wind.speed} km/h`;
-      minTempEl.innerHTML = `Minimum temperature: ${data.main.temp_min}° C`;
-      maxTempEl.innerHTML = `Maximum temperature: ${data.main.temp_min}° C`;
+      minTempEl.innerHTML = `Min: ${data.main.temp_min}° C`;
+      maxTempEl.innerHTML = `Max: ${data.main.temp_max}° C`;
       for (const item of data.weather) {
         detailsEl.innerHTML = item.description;
         iconEl.innerHTML = ` <img src="${iconWeatherURL}${item.icon}.png"alt=""></img>`;
@@ -41,33 +50,60 @@ function showCurrentWeather(location) {
     });
 }
 
-// function showCurrentWeather(location) {
-//   const url = `${currentWeatherURL}${location}`;
-//   fetch(url).then((response) =>
-//     response
-//       .json((response) => response.json())
-//       .then(
-//         (data) =>
-//           (weatherNowContainerEl.innerHTML = Object.entries(data)
-//             .map(
-//               (city) => `
-//               <div  class="weather-now">
-//               <h2>${city.name}</h2>
+forecastBtnEl.addEventListener("click", () => {
+  const location = inputCityEl.value;
+  if (location) {
+    showForecastedtWeather(location);
+  }
+});
 
-//               </div>
+function showForecastedtWeather(location) {
+  const forecastURL = `${forecastedWeatherURL}${location}`;
 
-//               `
-//             )
-//             .join(""))
-//       )
-//   );
-// }
+  fetch(forecastURL)
+    .then((response) => response.json())
+    .then((data) => {
+      data.list.forEach((item) => {
+        const dateAPI = item.dt_txt;
+        const date = new Date(dateAPI);
+        const options = {
+          weekday: "short",
+          hour: "numeric",
+          minute: "numeric",
+          hour12: false,
+        };
 
-// function showCurrentWeather(location) {
-//   const url = `${currentWeatherURL}${location}`;
-//   fetch(url).then((response) =>
-//     response
-//       .json((response) => response.json())
-//       .then((data) => console.log(Object.entries(data)))
-//   );
+        let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+        let name = weekDays[date.getDay()];
+        console.log(name);
+        let dayBlock = document.createElement("div");
+
+        dayBlock.innerHTML = `
+
+        <p class="day">${name}</p>
+        <p class="forecasted-temp">${item.main.temp}° C</p>
+        <p class="forecasted-description">${item.weather[0].description}</p>
+
+        
+        
+
+
+
+        `;
+        forecastedWeatherContainer.appendChild(dayBlock);
+
+        // dayBlock.innerHTML = console.log(date.toLocaleString("en-US", options));
+      });
+    });
+}
+
+// fetch(forecastURL)
+// .then((response) => response.json())
+// .then((data) => {
+//   for (let i = 0; i <= 39; i++) {
+//     const dateAPI = data.list[i].dt_txt;
+//     const date = new Date(dateAPI);
+//     console.log(date);
+//   }
+// });
 // }
